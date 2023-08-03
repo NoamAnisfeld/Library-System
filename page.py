b58ee35cab3e256c72b34e3546f0f34c849df057
Moshe Brizel <brizelmoshe@gmail.com>
2023-08-03 18:30:25 +0300

    move the all fanction to a class in ather page

--- page.py
+++ page.py
@@ -1,35 +1,11 @@
-from flask import Flask, redirect, url_for, render_template, request, flash
-import main
-app = Flask(__name__)
+from flask import Flask, render_template
 
+app = Flask(__name__)
 # the first page.
-@app.route("/", methods=["POST","GET"])
+@app.route("/")
 def index():
     # return the html page.
-    if request.method == "POST":
-        return redirect(url_for("add_book"))
-    else:
-        return render_template('index.html')
-@app.route("/add_book", methods=["POST","GET"])
-def add_book():
-    if request.method == "POST":
-        user = request.form
-        if not user["sum_books"].isdigit():
-            # flash("it need to be a number")
-            return render_template("add_books_page.html")
-        for i in user:
-            print(user[i])
-        user = tuple(user.values())
-        main.add_book(user)
-        print(user)
-        return redirect(url_for("book_adds", book=user))
-
-    else:
-        return render_template("add_books_page.html")
-
-@app.route("/<book>")
-def book_adds(book):
-    return f"<h1>{book}</h1>"
+    return render_template('index.html')
 #  turn on the website.
 if __name__ == '__main__':
     app.run(debug=True)
