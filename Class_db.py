8d0e95735a81514c1b0a49865fca56a7af1c1d4d
Moshe Brizel <brizelmoshe@gmail.com>
2023-08-09 12:34:28 +0300

    change the fanction that open the database in the fanction

--- Class_db.py
+++ Class_db.py
@@ -1,66 +1,95 @@
 import sqlite3
 class Class_db:
-    def __init__(self):
-        self.connection = sqlite3.connect("library_mego.db")
-        self.conn = self.connection.cursor()
 
     # To add a row to the database.
     def add_book(self, tuple_arg):
+        # open the conaction.
+        connection = sqlite3.connect("library_mego.db")
+        conn = connection.cursor()
         # the command to add.
-        self.conn.execute(f"INSERT INTO books (name_book, name_auther, genre, language, sum_books, location_book)"
-                        f"VALUES (?,?,?,?,?,?)", tuple_arg)
+        conn.execute(f"INSERT INTO books (name_book, name_auther, genre, language, sum_books, location_book)"
+                          f"VALUES (?,?,?,?,?,?)", tuple_arg)
         # save the changes
-        self.connection.commit()
+        connection.commit()
+        # close the conaction.
+        conn.close()
+        connection.close()
+
 
     # to show the all database.
     def show_entire_table(self, table_name):
+        # open the conaction.
+        connection = sqlite3.connect("library_mego.db")
+        conn = connection.cursor()
         # the command to select all
-        self.conn.execute(f"SELECT * "
+        conn.execute(f"SELECT * "
                      f"FROM {table_name}")
         # the result.
-        rows = self.conn.fetchall()
+        rows = conn.fetchall()
         for row in rows:
             print(row)
+        # close the conaction.
+        conn.close()
+        connection.close()
 
     def show_single_row(self, table_name, filter_by, filter_value):
+        # open the conaction.
+        connection = sqlite3.connect("library_mego.db")
+        conn = connection.cursor()
         try:
+
             # try to pynd the book
-            self.conn.execute(f"SELECT * "
+            conn.execute(f"SELECT * "
                          f"FROM {table_name} "
                          f"WHERE {filter_by} = ?;", (filter_value,))
             # the result.
-            rows = self.conn.fetchall()
+            rows = conn.fetchall()
             for book in rows:
                 print(book)
         except:
             print("name error")
+        # close the conaction.
+        conn.close()
+        connection.close()
+
 
     # to delete a book.
     def delete_book(self, table_name, filter_by, filter_value):
-
-        self.conn.execute(f"DELETE FROM {table_name}"
+        # open the conaction.
+        connection = sqlite3.connect("library_mego.db")
+        conn = connection.cursor()
+        conn.execute(f"DELETE FROM {table_name}"
                      f"WHERE {filter_by} = ?;", (filter_value,))
         # save the changes.
-        self.connection.commit()
+        connection.commit()
+        # close the conaction.
+        conn.close()
+        connection.close()
 
     def show_columns_data(self):
+        # open the conaction.
+        connection = sqlite3.connect("library_mego.db")
+        conn = connection.cursor()
         # to show the column.
-        columns = self.conn.execute("PRAGMA table_info(Books)")
+        columns = conn.execute("PRAGMA table_info(Books)")
         for col in columns:
             print(col)
-
+        # close the conaction.
+        conn.close()
+        connection.close()
 
     def delete_all_books(self, table_name):
+        # open the conaction.
+        connection = sqlite3.connect("library_mego.db")
+        conn = connection.cursor()
         # to delete all.
-        self.conn.execute(f"DELETE FROM {table_name}")
+        conn.execute(f"DELETE FROM {table_name}")
         # save the changes.
-        self.connection.commit()
+        connection.commit()
+        # close the conaction.
+        conn.close()
+        connection.close()
 
     def is_valid_column(self, column_name):
         column_names = ["num_book", "name_book", "name_auther", "genre", "language", "sum_books", "location_book"]
         return column_name in column_names
-
-    # to close the database
-    def close_the_db(self):
-        self.conn.close()
-        self.connection.close()
