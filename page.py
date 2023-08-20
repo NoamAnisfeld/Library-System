05a1d4ab959eb2cf185712096116f779c68d1920
Moshe Brizel <brizelmoshe@gmail.com>
2023-08-20 20:37:26 +0300

    page website

--- page.py
+++ page.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, redirect, url_for, render_template, request, flash
+import Class_db
 app = Flask(__name__)
+# to use the database.
+
+db = Class_db.Class_db()
 # the first page.
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
     # return the html page.
-    return render_template('index.html')
+    if request.method == "POST":
+        return redirect(url_for("add_book"))
+    else:
+        return render_template('library.html')
+@app.route("/add_book", methods=["POST","GET"])
+def add_book():
+    if request.method == "POST":
+        user = request.form
+        if not user["sum_books"].isdigit():
+            # flash("it need to be a number")
+            return render_template("add_books_page.html")
+        for i in user:
+            print(user[i])
+        user = tuple(user.values())
+        db.add_book(user)
+        print(user)
+        return redirect(url_for("book_adds", book=user))
+    else:
+        return render_template("add_books_page.html")
+
+@app.route("/<book>")
+def book_adds(book):
+    return f"<h1>{book}</h1>"
 #  turn on the website.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
