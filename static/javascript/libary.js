684c2e84e5ecb371a3054fc151c3ca56338eefbb
BenZionHasida <b0583216809@gmail.com>
2023-09-26 11:48:04 +0300

    fix some spelling mistakes

--- static/javascript/libary.js
+++ static/javascript/libary.js
@@ -1,15 +1,15 @@
 let listButtons = document.querySelectorAll(".blue-button");
 for (let index = 0; index < listButtons.length; index++) {
-  listButtons[index].addEventListener("mouseover", overBoxConect);
-  listButtons[index].addEventListener("mouseout", outBoxConect);
+  listButtons[index].addEventListener("mouseover", overBoxConnect);
+  listButtons[index].addEventListener("mouseout", outBoxConnect);
 }
 
 console.log(listButtons);
 
-function overBoxConect() {
+function overBoxConnect() {
   this.style.backgroundColor = "rgb(84, 105, 221)";
 }
 
-function outBoxConect() {
+function outBoxConnect() {
   this.style.backgroundColor = "rgb(19, 39, 153)";
 }
